0d89b8814d6d6095d1cf829836128915746a0a9e
Gabriel Vincent <gabriel@vincent.es>
2023-01-22 15:23:14 +0000

    v1.0.1

--- content.js
+++ content.js
@@ -1,10 +1,34 @@
-let currentIndex = 0
+let currentIndex = null
 
 const parentComments = Array.from(
   document.querySelectorAll('.comment-tree tr.athing.comtr td.ind[indent="0"]')
 ).map(td => td.closest('tr.athing.comtr'))
 
+function updateCurrentIndex() {
+  const currentScroll = window.scrollY
+  const currentComment = parentComments.find(
+    comment => comment.offsetTop >= currentScroll
+  )
+  currentIndex = parentComments.indexOf(currentComment)
+}
+
+function onScrollEnd(callback, refresh = 66) {
+  if (!callback || typeof callback !== 'function') return
+
+  let timeout
+
+  window.addEventListener(
+    'wheel',
+    function () {
+      window.clearTimeout(timeout)
+      timeout = setTimeout(callback, refresh)
+    },
+    false
+  )
+}
+
 document.addEventListener('keydown', e => {
+  let timeout
   if (
     document.activeElement.nodeName === 'INPUT' ||
     document.activeElement.nodeName === 'TEXTAREA'
@@ -12,13 +36,22 @@ document.addEventListener('keydown', e => {
     return
   }
 
+  if (e.key === 'ArrowUp' || e.key == 'ArrowDown') {
+    window.clearTimeout(timeout)
+    timeout = setTimeout(updateCurrentIndex, 66)
+  }
+
   if (e.key === 'ArrowLeft') {
+    if (currentIndex == null) return
     currentIndex = Math.max(0, currentIndex - 1)
   } else if (e.key === 'ArrowRight') {
-    currentIndex = Math.min(parentComments.length - 1, currentIndex + 1)
+    if (currentIndex == null) currentIndex = 0
+    else currentIndex = Math.min(parentComments.length - 1, currentIndex + 1)
   } else {
     return
   }
 
   parentComments[currentIndex].scrollIntoView({ behavior: 'smooth' })
 })
+
+onScrollEnd(updateCurrentIndex)
